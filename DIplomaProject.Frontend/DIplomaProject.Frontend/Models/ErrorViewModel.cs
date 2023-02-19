a26cc5c49c9984a36b61198dfd1ea6df80df09fd
sudokar <egor.tomashov@gmail.com>
2023-02-19 23:46:21 +0300

    Created

---
+++ DIplomaProject.Frontend/DIplomaProject.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DIplomaProject.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
