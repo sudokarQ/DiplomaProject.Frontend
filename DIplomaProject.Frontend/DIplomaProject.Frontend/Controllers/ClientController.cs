7c9a171f5ba006fd6690f86ebb3b97e167cbbb1d
Egor <e.tomashov@syberry.com>
2023-03-15 00:43:16 +0300

    Добавил контроллер и вью для получения клиентов

---
+++ DIplomaProject.Frontend/DIplomaProject.Frontend/Controllers/ClientController.cs
@@ -0,0 +1,34 @@
+﻿using DIplomaProject.Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DIplomaProject.Frontend.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetClients()
+        {
+            var apiUrl = "https://localhost:7161/GetAllClients";
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var clients = JsonConvert.DeserializeObject<List<ClientGetDto>>(content);
+                    ViewBag.Clients = clients;
+                    return View();
+                }
+                else
+                {
+                    return View("Error");
+                }
+            }
+        }
+    }
+}
---
+++ DIplomaProject.Frontend/DIplomaProject.Frontend/Models/ClientGetDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DIplomaProject.Frontend.Models
+{
+    public class ClientGetDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime? BirthdayDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string? Adress { get; set; }
+        public string? CompanyName { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
